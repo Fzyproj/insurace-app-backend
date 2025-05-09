d0ec6b94033f762250cca09adb79836ed9b408ae
lucfzy <lucfzy@163.com>
2025-05-09 23:05:38 +0800

    解决ResponseResult找不到的问题。

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
+from src.response.common_response import ResponseResult
+
 app = Flask(__name__)
 
 # 配置 SQLite 数据库地址
